c73519164cc0d582425f467d3e878ffd6fdef016
My Hang Hong <hhong@norquest.ca>
2025-02-26 17:38:16 -0700

    Update train.py

--- src/train.py
+++ src/train.py
@@ -172,3 +172,4 @@ class Trainer:
 if __name__ == "__main__":
     trainer = Trainer(config_path="configs/predict_config.yaml")
     trainer.train_pipeline()
+    subprocess.run(["mlflow", "server", "--host", "127.0.0.1", "--port", "5000"])
