eabac0adb353588c69eed10ab0c3832dc28d952b
Kiranpreet1150 <klnu72@norquest.ca>
2025-03-07 20:33:55 -0700

    Updated API script

--- src/Feature Importance.py
+++ src/Feature Importance.py
@@ -158,4 +158,4 @@ class FeatureAnalysis:
 if __name__ == "__main__":
     analyzer = FeatureAnalysis()
     analyzer.run_analysis()
-    subprocess.run(["mlflow", "server", "--host", "127.0.0.1", "--port", "6002"])
+    subprocess.run(["mlflow", "server", "--host", "127.0.0.1", "--port", "6004"])
